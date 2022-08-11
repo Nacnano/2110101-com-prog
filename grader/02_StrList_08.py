5f7bdf60901b8af1eb3f86242d35579d798dc393
nac <chotpisit080916c@gmail.com>
2022-08-11 23:39:09 +0700

    test

--- grader/02_StrList_08.py
+++ grader/02_StrList_08.py
@@ -1,18 +1,7 @@
-from fractions import Fraction
+from math import gcd
 
 a, b, c = input().replace(" ", "").split(",")
-num = 0
-if len(a) > 0:
-    num += int(a)
-if len(b) > 0:
-    num += int(b)*(0.1)**len(b)
-if len(c) > 0:
-    new_c = "0."+c*int(60/len(c))
-    num += (0.1)**len(b)*float(new_c)
-
-ls = str(Fraction(num).limit_denominator()).split("/")
-
-if len(ls) == 1:
-    print(ls[0] + " / " + "1")
-else:
-    print(ls[0] + " / " + ls[1])
+x = int(a+b+c)-int(a+b)
+y = 10**(len(b)+len(c))-10**(len(b))
+z = gcd(x, y)
+print(x//z, "/", y//z)

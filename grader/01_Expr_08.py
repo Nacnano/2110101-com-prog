de948b09341b10021310b12ddb1c9cd28253a002
nac <chotpisit080916c@gmail.com>
2022-08-11 22:58:35 +0700

    sleepy af

--- grader/01_Expr_08.py
+++ grader/01_Expr_08.py
@@ -5,6 +5,7 @@ def sqrt_n_times(x, n):
 def cube_root(y):
     return sqrt_n_times(y, 2)*sqrt_n_times(y, 4)*sqrt_n_times(y, 6)*sqrt_n_times(y, 8)*sqrt_n_times(y, 10)*sqrt_n_times(y, 12)*sqrt_n_times(y, 14)*sqrt_n_times(y, 16)
     # return sqrt_n_times(sqrt_n_times(sqrt_n_times(sqrt_n_times(sqrt_n_times(sqrt_n_times(sqrt_n_times(y, 2), 2), 2), 2), 2), 2), 2)
+    # what is 'x' for the regular calculator IDK
 
 
 def main():

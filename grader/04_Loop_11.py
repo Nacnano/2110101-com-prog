327f18e53ce0c3f8ae6b3e0b0b86c34e54a2d8c7
nac <chotpisit080916c@gmail.com>
2022-08-14 22:34:42 +0700

    done this problem with unclear statement BRUH

--- grader/04_Loop_11.py
+++ grader/04_Loop_11.py
@@ -1,18 +1,13 @@
-s = input()
-list_char = []
-list_count = []
+s = input() + '.'
+list_ans = []
 last = s[0]
 count = 1
 for i in range(1, len(s)):
     if s[i] == last:
         count += 1
     else:
-        list_char.append(last)
-        list_count.append(count)
+        list_ans.append((last, count))
         last = s[i]
         count = 1
-if s[-1] != s[-2]:
-    list_char.append(s[-1])
-    list_count.append(1)
-for i in range(len(list_char)):
-    print(list_char[i] + " " + str(list_count[i]) + " ", end="")
+for char, count in list_ans:
+    print(char + " " + str(count) + " ", end="")

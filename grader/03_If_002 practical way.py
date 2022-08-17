515d4e74377270f00b5d5799dca49d4fed8b29ac
nac <chotpisit080916c@gmail.com>
2022-08-17 23:15:52 +0700

    add real solution for Next15Days problem

--- grader/03_If_002 practical way.py
+++ grader/03_If_002 practical way.py
@@ -2,4 +2,4 @@ import datetime
 
 d, m, y = [int(x) for x in input().strip(" ").split(" ")]
 end_date = datetime.datetime(y-543, m, d) + datetime.timedelta(days=15)
-print(str(end_date.day) + "/" + str(end_date.month) + "/" + str(end_date.year+543))
+print("{}/{}/{}".format(end_date.day, end_date.month, end_date.year+543))

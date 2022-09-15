00abe209c91bb7ad474e14646287962726e9c230
nac <chotpisit080916c@gmail.com>
2022-09-15 21:58:31 +0700

    add DNA and python2 sucks because it can't handle f string format

--- grader/07_StrFile_31.py
+++ grader/07_StrFile_31.py
@@ -1,4 +1,4 @@
-s = input().strip()
+s = input().strip().upper()
 valid = True
 for c in s:
     if c not in ['A', 'T', 'C', 'G']:
@@ -9,7 +9,33 @@ q = input()
 if not valid:
     print("Invalid DNA")
 elif q == 'R':
-    s = s.replace('A', 'T').replace('T', 'A').replace('C', 'G').replace('G', 'C')
-    print(s)
+    new_s = ''
+    for c in s:
+        if c == 'A':
+            new_s += 'T'
+        elif c == 'T':
+            new_s += 'A'
+        elif c == 'C':
+            new_s += 'G'
+        else:
+            new_s += 'C'
+    print(new_s[::-1])
 elif q == 'F':
-    print("A=", s.count('A'), ", T=", s.count('T'), ", ")
+    print("A=", end='')
+    print(s.count('A'), end='')
+    print(", T=", end='')
+    print(s.count('T'), end='')
+    print(", G=", end='')
+    print(s.count('G'), end='')
+    print(", C=", end='')
+    print(s.count('C'))
+else:
+    target = input().strip()
+    ans = 0
+    for i in range(len(s)-len(target)+1):
+        add = 1
+        for j in range(len(target)):
+            if s[i+j] != target[j]:
+                add = 0
+        ans += add
+    print(ans)
--- grader/07_StrFile_33.py
+++ grader/07_StrFile_33.py
@@ -1,9 +1,9 @@
 def read_next(f):
     while True:
         t = f.readline()
-        if len(t) == 0:  # ถ้าอ่านหมดแล้ว ออกจากวงวน
+        if len(t) == 0:
             break
-        x = t.strip().split()  # ลบ blank ซ้ายขวา
-        if len(x) == 2:  # แยกแล้วได้ 2 ส่วน --> ถูกต้อง ก็คืนผล
+        x = t.strip().split()
+        if len(x) == 2:
             return x[0], x[1]
-    return "", ""  # แฟ้มหมดแล้ว คืนสตริงว่าง
+    return "", ""

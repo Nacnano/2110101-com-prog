c5bbcfcf9a51308b1af986f2d8e38814837ec063
nac <chotpisit080916c@gmail.com>
2022-09-16 09:20:20 +0700

    finish string file problems, start some dictionary problems

--- grader/07_StrFile_33.py
+++ grader/07_StrFile_33.py
@@ -1,9 +1,21 @@
-def read_next(f):
-    while True:
-        t = f.readline()
-        if len(t) == 0:
-            break
-        x = t.strip().split()
-        if len(x) == 2:
-            return x[0], x[1]
-    return "", ""
+file1, file2 = input().strip().split()
+list = []
+
+
+def read_file(filename):
+    global line
+    f = open(filename, 'r')
+    for line in f.readlines():
+        id, grade = line.strip().split()
+        faculty = id[-2:]
+        list.append([faculty, id, grade])
+    f.close()
+
+
+read_file(file1)
+read_file(file2)
+
+list = sorted(list)
+
+for faculty, id, grade in list:
+    print(id, grade)

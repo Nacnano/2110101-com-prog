191ea55b3676f298c7544a3d602231ad65bc08b6
nac <chotpisit080916c@gmail.com>
2022-09-16 20:07:04 +0700

    remove unused comments

--- grader/09_MoreDC_24.py
+++ grader/09_MoreDC_24.py
@@ -11,24 +11,3 @@ while s != 'q':
     s = input()
 for key in type_list:
     print(key + ': ' + ', '.join(dict[key]))
-
-
-# cartoon_name_type = dict()
-
-
-# inp = input()
-# type_order = []
-
-# while inp != "q":
-#     comp = inp.split(", ")
-#     name = comp[0]
-#     type = comp[1]
-#     if type not in cartoon_name_type:
-#         cartoon_name_type[type] = [name]
-#         type_order.append(type)
-#     else:
-#         cartoon_name_type[type] += [name]
-#     inp = input()
-
-# for t_order in type_order:
-#     print(t_order + ": " + ", ".join(cartoon_name_type[t_order]))

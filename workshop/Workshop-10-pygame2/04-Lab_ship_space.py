80785b1a814d66df4c1fa9ae5ad1c86943bcdb67
nac <chotpisit080916c@gmail.com>
2022-11-23 15:24:59 +0700

    feat(grader): add dump features

--- workshop/Workshop-10-pygame2/04-Lab_ship_space.py
+++ workshop/Workshop-10-pygame2/04-Lab_ship_space.py
@@ -12,11 +12,15 @@ FPS = 60
 white = (255, 255, 255)
 black = (0, 0, 0)
 
+
 # initialize pg and create window
 pg.init()
 screen = pg.display.set_mode((width, height))
 pg.display.set_caption("ShipSpace!")
 clock = pg.time.Clock()
+
+font_name = pg.font.match_font('arial')
+font = pg.font.Font(font_name, 30)
 
 # load game graphics
 bg = pg.image.load(path.join(img_dir, "space.png")).convert()
@@ -29,6 +33,8 @@ bullet_img = pg.image.load(path.join(img_dir, "red_bullet.png")).convert()
 # Class ที่เพิ่มเข้ามาใหม่ -> Ship , Meteor , Bullet ; Function ที่เพิ่มเข้ามาใหม่ -> newmeteor()
 
 # Class ของผู้เล่นภายในมี method __init__ , update , shoot
+
+
 class Ship(pg.sprite.Sprite):
     def __init__(self):
         pg.sprite.Sprite.__init__(self)
@@ -38,6 +44,7 @@ class Ship(pg.sprite.Sprite):
         self.rect.centerx = width / 2
         self.rect.bottom = height - 10
         self.speedx = 0
+        self.speedy = 0
 
     def update(self):
         self.speedx = 0
@@ -52,12 +59,25 @@ class Ship(pg.sprite.Sprite):
         if self.rect.left < 0:
             self.rect.left = 0
 
+        self.speedy = 0
+        if keystate[pg.K_UP]:
+            self.speedy = -8
+        if keystate[pg.K_DOWN]:
+            self.speedy = 8
+        self.rect.y += self.speedy
+        if self.rect.top < 0:
+            self.rect.top = 0
+        if self.rect.bottom > height:
+            self.rect.bottom = height
+
     def shoot(self):
         bullet = Bullet(self.rect.centerx, self.rect.top)
         all_sprites.add(bullet)
         bullets.add(bullet)
 
 # Class ของของหินภายในมี method __init__ , update
+
+
 class Meteor(pg.sprite.Sprite):
     def __init__(self):
         pg.sprite.Sprite.__init__(self)
@@ -78,6 +98,8 @@ class Meteor(pg.sprite.Sprite):
             self.speedy = random.randrange(1, 8)
 
 # Class ของลูกกระสุนภายในมี method __init__ , update
+
+
 class Bullet(pg.sprite.Sprite):
     def __init__(self, x, y):
         pg.sprite.Sprite.__init__(self)
@@ -95,18 +117,50 @@ class Bullet(pg.sprite.Sprite):
             self.kill()
 
 # function ที่สร้างหินขึ้นมา 1 ก้อน
+
+
 def newmeteor():
     m = Meteor()
     all_sprites.add(m)
     meteors.add(m)
 #################################################################################################
 
+
+score = 0
 new_game = True
 
 while True:
 
     # set ให้ตัวเกมส์แสดงผลด้วยความเร็วที่เหมาะสม
     clock.tick(FPS)
+    keystate = pg.key.get_pressed()
+    if keystate[pg.K_r]:
+        print("HOI")
+
+    try:
+        hits = pg.sprite.spritecollide(ship, meteors, False, False)
+        crash = len(hits)
+    except:
+        crash = False
+
+    # print(new_game, crash)
+    if crash == 1:
+        screen.fill(black)
+        text_surface = font.render("Score: "+str(score), True, white)
+        text_rect = text_surface.get_rect()
+        text_rect.topright = (width-10, 5)
+        screen.blit(text_surface, text_rect)
+        text_surface = font.render("Press [R] to Retry", True, white)
+        text_rect = text_surface.get_rect(center=(width/2, height/2))
+        screen.blit(text_surface, text_rect)
+        pg.display.flip()
+        if keystate[pg.K_r]:
+            print("YES")
+            new_game = True
+            crash = False
+            score = 0
+
+        continue
 
     if new_game:
         new_game = False
@@ -114,32 +168,51 @@ while True:
         #################################################################################################
         # TO DO 1-1 : สรา้ง sprite Group ให้กับ all_sprites, meteors, bullets, ship
 
-        all_sprites =
-        meteors =
-        bullets =
+        all_sprites = pg.sprite.Group()
+        meteors = pg.sprite.Group()
+        bullets = pg.sprite.Group()
 
         # TO DO 1-2 : สร้าง Object ให้กับ ship
-
+        ship = Ship()
 
         # TO DO 1-3 : เพิ่ม ship ลงใน all_sprites
-
+        all_sprites.add(ship)
 
         #################################################################################################
         # TO DO 2 : สร้าง Object Meteor ขึ้นมา 8 ก้อนโดยผ่านการเรียกใช้ฟังก์ชัน newmeteor()
-
-
-
+        for i in range(8):
+            newmeteor()
 
         #################################################################################################
+    screen.fill(black)
+    screen.blit(bg, bg_rect)
+    # bg_rect = screen
+    #################################################################################################
+    # TO DO 7 : วาด element ใน all_sprites ลงใน screen
+    all_sprites.draw(screen)
+    #################################################################################################
+    # after drawing everything, flip the display
+
+    text_surface = font.render("Score: "+str(score), True, white)
+    text_rect = text_surface.get_rect()
+    text_rect.topright = (width-10, 5)
+    screen.blit(text_surface, text_rect)
+    pg.display.flip()
+
+    #################################################################################################
+    # TO DO 6 : ตรวจสอบว่าหินชนยานผู้เล่นหรือไม่
+    # ถ้าชนให้เริ่มเกมใหม่
+
+    #################################################################################################
+    # TO DO 4 : clear screen ด้วยสีดำ จากนั้น เอา bg ใส่ใน bg_rect
 
     for event in pg.event.get():
         if event.type == pg.QUIT:
             pg.quit()
         #################################################################################################
         # TO DO 3 : ตรวจสอบว่าถ้ามีการกดปุ่ม spacebar (K_SPACE) ให้ ship เรียกฟังก์ชั่นสำหรับการยิงกระสุน
-
-
-
+        if keystate[pg.K_SPACE]:
+            ship.shoot()
 
         #################################################################################################
 
@@ -149,26 +222,7 @@ while True:
     #################################################################################################
     # TO DO 5 : ตรวจสอบว่าลูกกระสุนชนหินหรือไม่
     # ถ้าชนให้สร้างหินขึ้นมาใหม่เท่ากับจำนวนที่ถูกชนไป
-    hits =
-
-
-
-    #################################################################################################
-    # TO DO 6 : ตรวจสอบว่าหินชนยานผู้เล่นหรือไม่
-    # ถ้าชนให้เริ่มเกมใหม่
-    hits =
-    if hits:
-
-
-    #################################################################################################
-    # TO DO 4 : clear screen ด้วยสีดำ จากนั้น เอา bg ใส่ใน bg_rect
-
-
-    #################################################################################################
-    # TO DO 7 : วาด element ใน all_sprites ลงใน screen
-
-    #################################################################################################
-    # after drawing everything, flip the display
-    pg.display.flip()
-
-pg.quit()
+    hits = len(pg.sprite.groupcollide(bullets, meteors, True, True))
+    score += hits
+    for i in range(hits):
+        newmeteor()

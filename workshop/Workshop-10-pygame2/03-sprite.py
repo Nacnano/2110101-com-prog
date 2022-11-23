e68ad61962221fd5ac4d0c44cd62b0b0fb4aa8bc
nac <chotpisit080916c@gmail.com>
2022-11-23 13:49:27 +0700

    feat(workshop): complete tutorial

--- workshop/Workshop-10-pygame2/03-sprite.py
+++ workshop/Workshop-10-pygame2/03-sprite.py
@@ -4,28 +4,29 @@ pg.init()
 
 green = (20, 255, 140)
 white = (255, 255, 255)
-black = (0 , 0 , 0)
+black = (0, 0, 0)
 red = (255, 0, 0)
-teal = (0,128,128)
+teal = (0, 128, 128)
 
 width = 400
 height = 400
 
-screen = pg.display.set_mode((width,height))
+screen = pg.display.set_mode((width, height))
 pg.display.set_caption("ShipSpace")
 
+
 class Ship(pg.sprite.Sprite):
-    def __init__(self,color, width, height):
+    def __init__(self, color, width, height):
         super().__init__()
 
         # สร้าง Surface ขึ้นมาจากนั้นวาด "ยาน" ขึ้นมาด้วยสี่เหลี่ยมผืนผ้า
-        self.image = pg.Surface([30, 40])
-        self.image.fill(red)
+        # self.image = pg.Surface([30, 40])
+        # self.image.fill(red)
 
         # TO DO 1 : แทนที่เราจะใช้ shape เราสามารถใช้ภาพแทนได้
         # [Hint] : pg.image.load สำหรับโหลดภาพ , pg.transform.scale สำหรับปรับขนาดภาพ
-        
-        self.image= pg.transform.scale(self.image,(width,height))
+        self.image = pg.image.load("source/img/Ship.png")
+        self.image = pg.transform.scale(self.image, (width, height))
 
         # แปลง Surface เป็น object
         self.rect = self.image.get_rect()
@@ -37,49 +38,54 @@ class Ship(pg.sprite.Sprite):
         self.rect.x -= d
 
     # TO DO 2 : สร้าง function ที่สามารถทำให้ยาน ขยับขึ้น - ลง ได้
+    def moveUp(self, d):
+        self.rect.y -= d
 
-
-
-
+    def moveDown(self, d):
+        self.rect.y += d
 
 
 # TO DO 4 : สร้าง Class Meteor
-# โดยกำหนด attribute .image ไปยัง "source/img/meteor_med.png" 
+# โดยกำหนด attribute .image ไปยัง "source/img/meteor_med.png"
 # และกำหนด attribute .rect สำหรับ object
+class Meteor(pg.sprite.Sprite):
+    def __init__(self, color, width, height):
+        super().__init__()
 
+        # self.image = pg.Surface([30, 40])
+        # self.image.fill(red)
+        self.image = pg.image.load("source/img/meteor_med.png")
+        self.image = pg.transform.scale(self.image, (width, height))
 
-
-
-
+        self.rect = self.image.get_rect()
 
 
 # จะได้ list ที่ภายในมี sprites ที่ใช้ในเกมอยู่
 all_sprites = pg.sprite.Group()
 
-# TO DO 5-1 : สร้าง sprite Group ของ Meteor 
-
+# TO DO 5-1 : สร้าง sprite Group ของ Meteor
+meteors = pg.sprite.Group()
 
 # สร้าง Object ของยาน ขึ้นมา [ w = 50 , h = 38]
-myShip = Ship(red,50, 38)
+myShip = Ship(red, 50, 38)
 # กำหนดพิกัดเริ่มต้นของยาน
 myShip.rect.x = 200
 myShip.rect.y = 350
 
 
 # TO DO 5-2 : สร้าง Object meteor
-
+meteor = Meteor(red, 50, 50)
 
 # TO DO 5-3 : กำหนดพิกัดเริ่มต้นให้ Meteor [ x = 110 , y = 170]
-
+meteor.rect.x = 110
+meteor.rect.y = 170
 
 # เอา object Ship ที่สร้างขึ้นมาใส่ลงใน all_sprites
 all_sprites.add(myShip)
 
 # TO DO 5-4 : เอา object meteor ที่สร้างขึ้นมาใส่ลงใน all_sprites และ sprite group ที่เก็บ meteor
-
-
-
-
+all_sprites.add(meteor)
+meteors.add(meteor)
 
 # สร้าง clock
 clock = pg.time.Clock()
@@ -102,14 +108,13 @@ while True:
         myShip.moveRight(5)
 
     # TO DO 3 : ถ้ากด arrow ขึ้น ให้ขยับยานขึ้นไป 5 หน่วย และ ถ้ากด arrow ลง ให้ขยับยานลงมา 5 หน่วย
-
-
-
-
+    if keystate[pg.K_UP]:
+        myShip.moveUp(5)
+    elif keystate[pg.K_DOWN]:
+        myShip.moveDown(5)
 
     # TO DO 6 : ตรวจสอบว่าถ้ายานชนหิน ให้ทำลายหินก้อนนั้นซะ
-
-
+    pg.sprite.spritecollide(myShip, meteors, True)
 
     # ใส่พื้นหลังสีเขียว
     screen.fill(green)
